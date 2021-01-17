e02adc251f54a33c1db094b76d120fe14228ff30
Alexey Solovyov <plotinka@lyoha.info>
2021-01-17 19:08:59 +0300

    feat: ability to upload single file with fixed name

--- c/quickwebserver-utils.h
+++ c/quickwebserver-utils.h
@@ -64,3 +64,73 @@ void readFile(FileData *file, const char *path) {
       file->size = 0;
   }
 }
+
+char *cut_string(const char *buffer, const char *start_from, char end_char) {
+  char *start_with = strstr(buffer, start_from);
+  short start_pos = start_with - buffer + strlen(start_from);
+  short idx = 0;
+  char *result = malloc(sizeof(char *) * idx);
+  while (1)
+  {
+    char letter = buffer[start_pos + idx];
+    if (letter == end_char) {
+      result = realloc(result, sizeof(char*) * (idx - 1));
+      result[idx] = '\0';
+      break;
+    }
+    result = realloc(result, sizeof(char*) * (idx));
+    result[idx] = letter;
+    idx++;
+  }
+  return result;
+}
+
+// @TODO: parse multiple files
+// @TODO: parse rest body
+void getMultipartFile(const char *buffer, size_t buf_length, char *content_type) {
+  char *boundary = cut_string(content_type, "boundary=", '\n');  
+  char b_end[strlen(boundary) + 5];
+  snprintf(b_end, sizeof b_end, "%s%s%s", "--", boundary, "--");
+  char *boundary_closing = b_end;
+
+  char *ptr = strtok(buffer, "\r\n");
+  short content_next = 0;
+  while(ptr != NULL)
+	{
+    if (strlen(ptr) == 1 && (ptr[0] == '\r' || ptr[0] == '\n')) {
+      content_next = 1;
+    }
+    if (content_next == 1) {
+      break;
+    }
+    		ptr = strtok(NULL, "\n\n");
+	}
+
+  size_t boundary_start = 0;
+  size_t file_end = 0;
+  size_t i = 0;
+  while(1) {
+    char byte_v = ptr[i];
+
+    if (boundary_start == 0 && byte_v == boundary_closing[0]) {
+      boundary_start = i;
+    }
+    
+    if (boundary_start != 0 && boundary_closing[i - boundary_start] == byte_v) {
+      if (i - boundary_start == strlen(boundary_closing) - 1) {
+        file_end = i - strlen(boundary_closing) - 1;
+        break;
+      }
+    } else {
+      boundary_start = 0;
+    }
+    i++;
+  }
+
+  // @todo: real file name
+  FILE *resFile = fopen("./test.png", "wb");
+  for (int i = 2; i < file_end ; i++) {
+    fputc(ptr[i], resFile);
+  }
+  fclose(resFile);
+}
--- c/quickwebserver.c
+++ c/quickwebserver.c
@@ -58,8 +58,14 @@ JSValue parseHttp(struct http_request_s* request) {
                                   JS_NewString(QWS.serverContext, request_method),
                                   JS_PROP_C_W_E);
 
+	// Getting content type
+	http_string_t contentTypeHeader = http_request_header(request, "Content-Type");
+	char request_content_type[contentTypeHeader.len];
+	stringSlice(request_content_type, contentTypeHeader.buf, contentTypeHeader.len);
+
 	// Getting request body
 	http_string_t body = http_request_body(request);
+	getMultipartFile(body.buf, body.len, request_content_type);
 	JS_DefinePropertyValueStr(QWS.serverContext, returnObject, "body",
                                   JS_NewString(QWS.serverContext, body.buf),
                                   JS_PROP_C_W_E);
